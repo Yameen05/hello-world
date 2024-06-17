75f013d314642237310054deb32a318f37c52a8d
Yameen05 <103006919+Yameen05@users.noreply.github.com>
2024-06-17 18:15:21 -0400

    starter.java

--- hello-world.java
+++ hello-world.java
@@ -1,15 +1,7 @@
-/**
- * Calculates the BMI of the policyholder.
- * @return the BMI as a double.
- */
 public double calculateBMI() {
-    // Method implementation
+
 }
 
-/**
- * Gets the policy number.
- * @return the policy number as an int.
- */
 public int getPolicyNumber() {
     return policyNumber;
 }
